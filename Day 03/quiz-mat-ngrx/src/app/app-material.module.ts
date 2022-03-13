66f8e6fb476d3b0b36585d7dfa3eb26e74792d5f
Kobi Hari <kobi2294@yahoo.com>
2022-03-13 13:56:10 +0200

    before building the real UI

--- Day 03/quiz-mat-ngrx/src/app/app-material.module.ts
+++ Day 03/quiz-mat-ngrx/src/app/app-material.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 const exportables = [
-    MatButtonModule
+  MatButtonModule,
+  MatIconModule,
+  MatCardModule,
+  MatListModule,
+  MatToolbarModule
 ];
 
 @NgModule({
-    imports: exportables, 
-    exports: exportables
+  imports: exportables,
+  exports: exportables,
 })
-export class AppMaterialModule {
-}
+export class AppMaterialModule {}
